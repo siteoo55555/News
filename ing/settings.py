d2babd02f15dec3fe2f47c019fdec57caf235a46
some <some@gmail.com>
2024-07-01 16:13:07 +0500

    7-commit

--- ing/settings.py
+++ ing/settings.py
@@ -132,7 +132,7 @@ MEDIA_ROOT = BASE_DIR  /  'media'
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS =[BASE_DIR / 'staticfiles']
+# STATICFILES_DIRS =[BASE_DIR / 'staticfiles']
 STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
