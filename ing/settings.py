ce5ea2fa788abd3691ceea07919d428cc864dbc0
some <some@gmail.com>
2024-07-04 15:36:35 +0500

    ok

--- ing/settings.py
+++ ing/settings.py
@@ -132,7 +132,6 @@ MEDIA_ROOT = BASE_DIR  /  'media'
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATICFILES_DIRS =[BASE_DIR / 'staticfiles']
 STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
